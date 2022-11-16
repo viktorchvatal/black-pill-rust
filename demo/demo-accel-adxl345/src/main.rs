9ef9a988e34e48d66a734e3de7ec796bf02c0567
Viktor Chvatal <viktor@chvatal.name>
2022-11-16 21:38:57 +0100

    Basic text accelerometer demo.

--- demo/demo-accel-adxl345/src/main.rs
+++ demo/demo-accel-adxl345/src/main.rs
@@ -3,17 +3,17 @@
 
 use core::fmt::Write;
 use arrayvec::ArrayString;
-use cortex_m::peripheral::DWT;
 use cortex_m_rt::{entry};
 use embedded_graphics::{
     pixelcolor::BinaryColor,
     prelude::*,
-    primitives::{PrimitiveStyle, Circle}, mono_font::{MonoTextStyle, ascii::FONT_7X13_BOLD}, text::Text
+    mono_font::{MonoTextStyle, ascii::{FONT_5X8, FONT_7X13_BOLD}}, text::Text
 };
 use embedded_hal::spi;
 use panic_halt as _;
 use sh1106::{prelude::*, Builder, interface::DisplayInterface};
-use stm32f4xx_hal::{prelude::*, pac, gpio::NoPin, time::Hertz};
+use stm32f4xx_hal::{prelude::*, pac, gpio::NoPin, i2c::I2c};
+use adxl343::{Adxl343, accelerometer::Accelerometer};
 
 #[entry]
 fn main() -> ! {
@@ -52,7 +52,6 @@ fn run(
     );
 
     let mut display_reset = gpiob.pb14.into_push_pull_output();
-    let mut led = gpioc.pc13.into_push_pull_output();
     let mut delay = dp.TIM5.delay_us(&clocks);
 
     let mut display: GraphicsMode<_> = Builder::new()
@@ -64,9 +63,40 @@ fn run(
     display.reset(&mut display_reset, &mut delay).map_err(|_| ())?;
     display.init().unwrap();
 
+    let mut i2c = I2c::new(
+        dp.I2C1,
+        (
+            gpiob.pb8.into_alternate().set_open_drain(),
+            gpiob.pb9.into_alternate().set_open_drain(),
+        ),
+        400.kHz(),
+        &clocks,
+    );
+
+    let mut accelerometer = match Adxl343::new(i2c) {
+        Ok(device) => device,
+        Err(_error) => stop_on_error(
+            display, "Accelerometer demo\nFailed to\ninitialize"
+        )
+    };
+
     loop {
         display.clear();
-        print(&mut display, "Hello\nAccelerometer demo")?;
+        match accelerometer.accel_norm() {
+            Ok(values) => {
+                let mut text = ArrayString::<100>::new();
+                let _ = write!(
+                    &mut text,
+                    "Accelerometer demo\nX = {}\nY = {}\nZ = {}",
+                    values.x, values.y, values.z
+                );
+                let _ = print(&mut display, &text);
+            },
+            Err(_error) => {
+                let _ = print(&mut display, "Read error");
+            },
+        };
+
         display.flush().unwrap();
     }
 }
@@ -80,4 +110,16 @@ where T: DisplayInterface{
     let position = Point::new(0, 8);
     Text::new(&message, position, style).draw(display).map_err(|_| ())?;
     Ok(())
+}
+
+fn stop_on_error<T>(
+    mut display: GraphicsMode<T>,
+    message: &str
+) -> !
+where T: DisplayInterface{
+    let style = MonoTextStyle::new(&FONT_7X13_BOLD, BinaryColor::On);
+    let position = Point::new(0, 8);
+    let _ = Text::new(&message, position, style).draw(&mut display);
+    let _ = display.flush();
+    loop {}
 }
